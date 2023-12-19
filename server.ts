3a7eac11e633431526448bfca4cd5159621a4536
IvanS <i.surovtsev22@gmail.com>
2023-12-19 14:41:16 +0500

    some updates

---
+++ server.ts
@@ -0,0 +1,29 @@
+import { createServer } from "http";
+import cors from "cors";
+import express from "express";
+import { Server } from "socket.io";
+import { SOCKET_SEND_THEME, SOCKET_RECEIVE_THEME } from "./src/constants";
+
+const app = express();
+
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on(SOCKET_SEND_THEME, (data) => {
+    socket.broadcast.emit(SOCKET_RECEIVE_THEME, data);
+  });
+});
+
+const port = process.env.PORT || 3001;
+
+httpServer.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
